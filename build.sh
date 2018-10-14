742915b51fc4ebef110c57f089f75f9a95c44cc4
Akira Chiku <akira.chiku@gmail.com>
2018-10-14 13:06:42 +0900

    Add build script

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+GOOS=windows GOARCH=amd64 go build -o pkg/shipmenttimes-amd64.exe
+GOOS=windows GOARCH=386 go build -o pkg/shipmenttimes-386.exe
+GOOS=darwin GOARCH=amd64 go build -o pkg/shipmenttimes-darwin-amd64
+GOOS=darwin GOARCH=386 go build -o pkg/shipmenttimes-darwin-386
