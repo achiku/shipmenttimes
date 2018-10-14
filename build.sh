f89a5dc24b9660578862cc316f8c7a2905667453
Akira Chiku <akira.chiku@gmail.com>
2018-10-14 13:17:08 +0900

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+rm -r ./pkg
+mkdir ./pkg
+
 GOOS=windows GOARCH=amd64 go build -o pkg/shipmenttimes-amd64.exe
 GOOS=windows GOARCH=386 go build -o pkg/shipmenttimes-386.exe
 GOOS=darwin GOARCH=amd64 go build -o pkg/shipmenttimes-darwin-amd64
